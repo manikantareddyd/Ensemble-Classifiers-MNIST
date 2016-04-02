e32703ca4b3c6c11ece1fca971acca4df21deb7b
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-04-02 15:06:46 +0530

    Now generating Data with Equal Distribution of All Classes

--- src/data.py
+++ src/data.py
@@ -11,6 +11,7 @@ class Data:
         self.learning_set = []
         self.learning_set_labels = []
         self.load(pixels_per_cell,cells_per_block,orientations)
+
     def load(self,pixels_per_cell = (8,8),cells_per_block=(3,3),orientations=9):
         '''
         Generates a Data Set
@@ -29,27 +30,41 @@ class Data:
         print "Loaded Raw images"
 
         learning_set = []
+        Boom = {}
+        for i in range(10):
+            Boom[str(i)] = []
         for i in range(0,60000):
-            learning_set.append((train_raw[0][i],train_raw[1][i]))
+            Boom[str(train_raw[1][i])].append(train_raw[0][i])
         for i in range(0,10000):
-            learning_set.append((test_raw[0][i],test_raw[1][i]))
+            Boom[str(test_raw[1][i])].append(test_raw[0][i])
+        t = datetime.now().microsecond
+        random.seed(t)
+        [random.shuffle(Boom[str(i)]) for i in range(10)]
 
         print "Choosing 20000 training images uniformly randomly"
 
+        # Descriptor Generator
+        for l in range(10):
+            for i in range(0,2000):
+                img =  np.array(Boom[str(l)][i])
+                img.shape = (28,28)
+                fd, hog_image = hog(img, orientations=orientations, pixels_per_cell=pixels_per_cell,cells_per_block=cells_per_block, visualise=True)
+                learning_set.append([fd,l])
+
+        print "Data Points now chosen and Generated HOG descriptors for them"
+
         t = datetime.now().microsecond
         random.seed(t)
+        print "Shuffling Chosen Data Set"
         random.shuffle(learning_set)
-        print "Chosen"
-        # Descriptor Generator
-        for i in range(0,20000):
-            img =   np.array(learning_set[i][0])
-            img.shape = (28,28)
-            fd, hog_image = hog(img, orientations=orientations, pixels_per_cell=pixels_per_cell,cells_per_block=cells_per_block, visualise=True)
-            self.learning_set.append(fd)
+
+        for i in range(20000):
+            self.learning_set.append(learning_set[i][0])
             self.learning_set_labels.append(learning_set[i][1])
 
         print "Data Loading and Distribution Succesfully done"
-        # self.train_set = self.learning_set[:10000]
-        # self.train_labels = self.learning_set_labels[:10000]
-        # self.test_set = self.learning_set[10000:20000]
-        # self.test_labels = self.learning_set_labels[10000:20000]
+
+        self.train_set = self.learning_set[:10000]
+        self.train_labels = self.learning_set_labels[:10000]
+        self.test_set = self.learning_set[10000:20000]
+        self.test_labels = self.learning_set_labels[10000:20000]
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from classifiers import *
 
 Data = Data()
 f=open('Results/DTC.txt','w')
-for depth in range(10,20):
+for depth in range(10,11):
     r=RFC(Data,depth)
     f.write(str(depth)+' '+str(r.score)+'\n')
 
